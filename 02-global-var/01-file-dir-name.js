75ee478a41aa65c3c15cc5924112855299330183
zhaoxiaoxiao <1877145940@qq.com>
2019-09-04 21:49:44 +0800

    add

---
+++ 02-global-var/01-file-dir-name.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+console.log('dir name:',__dirname);
+console.log('file name:',__filename);
+
+
+//opterate data file
+
+var file=__dirname + '/data/db.xml';
+console.log('file name:',file);
+
+file=__dirname+'\\data\\db.xml';
+console.log('file name in windows:',file);
+
+
+const path=require('path');
+
+file=path.join(__dirname,'data','db.xml');
+console.log(file);
---
+++ 03-console/01-format.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+var wd = {
+   'Name':'赵肖肖',
+   'QQ':'1877145940',
+   'Age':'21'
+   
+};
+
+
+console.log('Name:%s',wd.Name);
+console.log('Age:%d',wd.Age);
+console.log('Zhaoxiaoxiao Info:',wd);
+
+console.log('Name:',wd.Name);
+console.log('Age is ${wd.Age}');
