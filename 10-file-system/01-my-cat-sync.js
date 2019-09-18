6e6eff597eeac4605589b58c4bf2f49a10d1fe17
zhaoxiaoxiao <1877145940@qq.com>
2019-09-18 15:56:46 +0800

    add

---
+++ 10-file-system/01-my-cat-sync.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const fs=require('fs'),
+      file=process.argv[2] ||__filename;
+if(fs.existsSync(file)){
+
+console.log(fs.readFileSync(file).toString('utf8'));
+}
+else{
+  consle.error('%s not exits!',file);
+  process.exit(1);
+}
+
+
+
